0c427a849f4ccb1581085e00449606e8cdef1fd3
satpamrobot-beep <satpamrobot@gmail.com>
2026-06-03 07:06:24 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,19 +2,15 @@ import asyncio
 from aiogram import Bot, Dispatcher
 
 from config import BOT_TOKEN
-from database import db
-
-from handlers import admin, moderation
+from handlers import admin, moderation, welcome
 
 async def main():
     bot = Bot(token=BOT_TOKEN)
     dp = Dispatcher()
 
-    await db.connect()
-    await db.init()
-
     dp.include_router(admin.router)
     dp.include_router(moderation.router)
+    dp.include_router(welcome.router)
 
     print("Bot running...")
     await dp.start_polling(bot)
