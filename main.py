058f1c25fa2c6c663bbe78a975a6c4c8f96edcd5
satpamrobot-beep <satpamrobot@gmail.com>
2026-06-03 07:57:46 +0700

    Update main.py

--- main.py
+++ main.py
@@ -5,15 +5,23 @@ from config import BOT_TOKEN
 from handlers import admin, moderation, welcome
 
 async def main():
+
+    print("🚀 Starting bot...")
+
     bot = Bot(token=BOT_TOKEN)
     dp = Dispatcher()
 
+    # routers
     dp.include_router(admin.router)
     dp.include_router(moderation.router)
     dp.include_router(welcome.router)
 
-    print("Bot running...")
+    print("🤖 Bot running...")
+
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    try:
+        asyncio.run(main())
+    except Exception as e:
+        print("❌ CRASH ERROR:", e)
