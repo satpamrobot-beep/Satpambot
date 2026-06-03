9d7c061e1db4d56ee73a6c8c7970198a7178cafb
satpamrobot-beep <satpamrobot@gmail.com>
2026-06-04 00:21:35 +0700

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from aiogram import Bot, Dispatcher
 from config import BOT_TOKEN
 
 from handlers import admin, moderation, welcome, start, help
+from handlers.broadcast import router as broadcast_router
+
 
 # ⚠️ IMPORT PANEL HARUS ADA FILE-NYA
 from handlers.panel import router as panel_router
@@ -19,6 +21,7 @@ async def main():
 
     dp.include_router(start.router)
     dp.include_router(admin.router)
+    dp.include_router(broadcast.router)
     dp.include_router(moderation.router)
     dp.include_router(welcome.router)
     dp.include_router(help.router)
