f8c3480854bbb0cda9bb76ffe2e7690626e52d6b
satpamrobot-beep <satpamrobot@gmail.com>
2026-06-04 00:28:07 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,36 +2,99 @@ import asyncio
 import logging
 
 from aiogram import Bot, Dispatcher
+
 from config import BOT_TOKEN
 
-from handlers import admin, moderation, welcome, start, help
-from handlers.broadcast import router as broadcast_router
+from database import db
 
+from handlers import (
+    admin,
+    moderation,
+    welcome,
+    start,
+    help
+)
 
-# ⚠️ IMPORT PANEL HARUS ADA FILE-NYA
-from handlers.panel import router as panel_router
+from handlers.group_events import (
+    router as group_events_router
+)
 
+from handlers.broadcast import (
+    router as broadcast_router
+)
 
-logging.basicConfig(level=logging.INFO)
+from handlers.panel import (
+    router as panel_router
+)
+
+logging.basicConfig(
+    level=logging.INFO
+)
+
 
 async def main():
 
-    bot = Bot(token=BOT_TOKEN)
+    await db.connect()
+
+    bot = Bot(
+        token=BOT_TOKEN
+    )
+
     dp = Dispatcher()
 
-    dp.include_router(start.router)
-    dp.include_router(admin.router)
-    dp.include_router(broadcast.router)
-    dp.include_router(moderation.router)
-    dp.include_router(welcome.router)
-    dp.include_router(help.router)
+    # =========================
+    # ROUTERS
+    # =========================
 
-    dp.include_router(panel_router)
+    dp.include_router(
+        start.router
+    )
 
-    print("🤖 Bot running...")
+    dp.include_router(
+        group_events_router
+    )
 
-    await dp.start_polling(bot)
+    dp.include_router(
+        admin.router
+    )
+
+    dp.include_router(
+        moderation.router
+    )
+
+    dp.include_router(
+        welcome.router
+    )
+
+    dp.include_router(
+        help.router
+    )
+
+    dp.include_router(
+        broadcast_router
+    )
+
+    dp.include_router(
+        panel_router
+    )
+
+    print(
+        "🤖 Bot running..."
+    )
+
+    try:
+
+        await dp.start_polling(
+            bot
+        )
+
+    finally:
+
+        await db.close()
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+
+    asyncio.run(
+        main()
+    )
