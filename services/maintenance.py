2e2dee59e160fcbd6cf1270ef4529711b7a8338c
satpamrobot-beep <satpamrobot@gmail.com>
2026-06-05 09:25:24 +0700

    Delete services directory

--- services/maintenance.py
+++
@@ -1,8 +0,0 @@
-from config import MAINTENANCE_MODE
-
-def set_maintenance(value: bool):
-    global MAINTENANCE_MODE
-    MAINTENANCE_MODE = value
-
-def get_maintenance():
-    return MAINTENANCE_MODE
