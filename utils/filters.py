97c08c6443f3ba52b43e59fff1ff6c9c702beaf8
satpamrobot-beep <satpamrobot@gmail.com>
2026-06-05 09:25:32 +0700

    Delete utils directory

--- utils/filters.py
+++
@@ -1,8 +0,0 @@
-import re
-
-def contains_link(text: str):
-    if not text:
-        return False
-
-    pattern = r"http[s]?://|t\.me/|www\."
-    return bool(re.search(pattern, text))
--- utils/maintenance_guard.py
+++
@@ -1,7 +0,0 @@
-from config import MAINTENANCE_MODE, MAINTENANCE_TEXT
-
-async def check_maintenance(message):
-    if MAINTENANCE_MODE:
-        await message.reply(MAINTENANCE_TEXT, parse_mode="HTML")
-        return True
-    return False
