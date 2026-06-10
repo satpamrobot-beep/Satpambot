318418f9886f848661b3d2d1c4d89ab6ac86f7a5
satpamrobot-beep <satpamrobot@gmail.com>
2026-06-10 17:24:19 +0700

    Update router.py

--- bot/router.py
+++ bot/router.py
@@ -1,3 +1,11 @@
 from aiogram import Router
 
+from bot.handlers.start import router as start_router
+from bot.handlers.admin import router as admin_router
+from bot.handlers.callback import router as callback_router
+
 router = Router()
+
+router.include_router(start_router)
+router.include_router(admin_router)
+router.include_router(callback_router)
