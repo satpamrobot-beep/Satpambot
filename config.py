204097769e81a949d50534d645dc322a71eeb967
satpamrobot-beep <satpamrobot@gmail.com>
2026-06-11 11:57:29 +0700

    Update config.py

--- config.py
+++ config.py
@@ -8,32 +8,32 @@ load_dotenv()
 # CORE BOT
 # =========================
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-
 DATABASE_URL = os.getenv("DATABASE_URL")
-CHANNEL_DB = os.getenv("CHANNEL_DB")
-SUPABASE_URL = os.getenv("SUPABASE_URL")
-SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+
+
 # =========================
 # OWNER & ADMINS
 # =========================
-OWNER_ID = int(os.getenv("OWNER_ID", "0"))
+OWNER_ID = int(os.getenv("OWNER_ID") or 0)
 
-ADMINS = set(
-    int(x) for x in os.getenv("ADMINS", "").split(",") if x.strip().isdigit()
-)
+ADMINS = {
+    int(x)
+    for x in os.getenv("ADMINS", "").split(",")
+    if x.strip().isdigit()
+}
 
 
 # =========================
 # FORCE JOIN CONFIG
 # =========================
-FORCE_CHANNEL = int(os.getenv("FORCE_CHANNEL", "0"))
-GROUP_ID = int(os.getenv("GROUP_ID", "0"))  # ⚠️ penting kamu belum punya ini di config
+FORCE_CHANNEL = int(os.getenv("FORCE_CHANNEL") or 0)
+GROUP_ID = int(os.getenv("GROUP_ID") or 0)
 
-UPDATE_CHANNEL = os.getenv("UPDATE_CHANNEL")
-NOTIFICATION_CHANNEL = int(os.getenv("NOTIFICATION_CHANNEL", "0"))
+UPDATE_CHANNEL = os.getenv("UPDATE_CHANNEL", "")
+NOTIFICATION_CHANNEL = int(os.getenv("NOTIFICATION_CHANNEL") or 0)
 
 
 # =========================
 # VIP
 # =========================
-VIP_LINK = os.getenv("VIP_LINK")
+VIP_LINK = os.getenv("VIP_LINK", "")
