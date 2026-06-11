90474f33c485ab25e5f8e06ab465f82165098175
satpamrobot-beep <satpamrobot@gmail.com>
2026-06-11 10:23:35 +0700

    Update supabase.py

--- db/supabase.py
+++ db/supabase.py
@@ -1,4 +1,4 @@
 from supabase import create_client
-from config import DATABASE_URL, CHANNEL_DB
+from config import SUPABASE_URL, SUPABASE_KEY
 
-supabase = create_client(DATABASE_URL, CHANNEL_DB)
+supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
