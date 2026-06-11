2d33733b78da0a7d1c3bba5a4a846694cd237cdb
satpamrobot-beep <satpamrobot@gmail.com>
2026-06-11 10:31:21 +0700

    Update config.py

--- config.py
+++ config.py
@@ -11,8 +11,6 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 CHANNEL_DB = os.getenv("CHANNEL_DB")
-SUPABASE_URL = os.getenv("SUPABASE_URL")
-SUPABASE_KEY = os.getenv("SUPABASE_KEY")
 
 # =========================
 # OWNER & ADMINS
