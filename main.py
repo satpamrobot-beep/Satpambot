4d6ed9adce66c1278ea2c7bf22c098d70ade4185
satpamrobot-beep <satpamrobot@gmail.com>
2026-06-11 10:29:29 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,54 +1,26 @@
 import asyncio
-import logging
-
 from aiogram import Bot, Dispatcher
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 
 from config import BOT_TOKEN
-from db.supabase import supabase  # optional check import
-
-# handlers router
-from handlers.start import router as start_router
-from handlers.callback import router as callback_router
-from handlers.code import router as code_router
-from handlers.admin.panel import router as admin_router
+from db.pool import init_db
+from bot.router import router
 
 
-# =========================
-# LOGGING
-# =========================
-logging.basicConfig(
-    level=logging.INFO,
-    format="%(asctime)s | %(levelname)s | %(message)s"
-)
-
-
-# =========================
-# BOT INIT
-# =========================
-bot = Bot(
-    token=BOT_TOKEN,
-    default=DefaultBotProperties(parse_mode=ParseMode.HTML)
-)
-
-dp = Dispatcher()
-
-
-# =========================
-# REGISTER ROUTERS
-# =========================
-dp.include_router(start_router)
-dp.include_router(callback_router)
-dp.include_router(code_router)
-dp.include_router(admin_router)
-
-
-# =========================
-# START BOT
-# =========================
 async def main():
-    logging.info("🚀 Bot is starting...")
+    bot = Bot(
+        token=BOT_TOKEN,
+        default=DefaultBotProperties(parse_mode=ParseMode.HTML)
+    )
+
+    dp = Dispatcher()
+
+    await init_db()
+
+    dp.include_router(router)
+
+    print("🔥 BOT RUNNING")
 
     await dp.start_polling(bot)
 
