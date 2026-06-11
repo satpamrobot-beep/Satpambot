2202b28dce3df7c28f6ecef3b390aa3d7f4da3a3
satpamrobot-beep <satpamrobot@gmail.com>
2026-06-11 12:09:40 +0700

    Update router.py

--- bot/router.py
+++ bot/router.py
@@ -6,8 +6,9 @@ router = Router()
 # IMPORT HANDLERS
 # =========================
 from handlers.start import router as start_router
-
+from handlers.callbacks import router as cb_router
 # =========================
 # INCLUDE ROUTERS
 # =========================
 router.include_router(start_router)
+router.include_router(cb_router
