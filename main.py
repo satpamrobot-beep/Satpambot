ea573e28eb3e6c9739abca0096fe283cdd3a6e37
satpamrobot-beep <satpamrobot@gmail.com>
2026-06-13 04:46:46 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,33 +1,20 @@
 import asyncio
-from aiogram import Bot, Dispatcher
+from aiogram import Dispatcher
 from bot.loader import bot, dp
 
-from bot.db.database import connect_db, close_db
-
-# import router semua handler (nanti kamu tambah satu-satu)
+from bot.db.database import init_db, close_db
 from bot.handlers import start
 
 
 async def main():
-    # 🔥 CONNECT DATABASE DULU
-    await connect_db()
+    await init_db()  # 🔥 CONNECT DB
 
-    # 🔥 REGISTER ROUTER
     dp.include_router(start.router)
 
     print("🤖 EarnFile Bot Running...")
 
-    # 🔥 START BOT
     await dp.start_polling(bot)
 
 
-# optional cleanup
-async def on_shutdown():
-    await close_db()
-
-
 if __name__ == "__main__":
-    try:
-        asyncio.run(main())
-    except KeyboardInterrupt:
-        print("Bot stopped")
+    asyncio.run(main())
