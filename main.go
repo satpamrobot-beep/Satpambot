f293665d4f5e1dbfae00c78ce9560100d594529e
satpamrobot-beep <satpamrobot@gmail.com>
2026-06-13 20:25:50 +0700

    Update main.go

--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
 	fmt.Println("Bot Online")
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "OK")
+	})
+
+	http.ListenAndServe(":8080", nil)
 }
