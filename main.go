7db511a147fff0febe0a22e6a1e59a30682f50cb
satpamrobot-beep <satpamrobot@gmail.com>
2026-06-13 20:14:40 +0700

    Update main.go

--- main.go
+++ main.go
@@ -1,37 +1,7 @@
 package main
 
-import (
-	"context"
-	"log"
-	"os"
-
-	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
-	"github.com/jackc/pgx/v5/pgxpool"
-)
+import "fmt"
 
 func main() {
-
-	pool, err := pgxpool.New(
-		context.Background(),
-		os.Getenv("DATABASE_URL"),
-	)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	defer pool.Close()
-
-	bot, err := tgbotapi.NewBotAPI(
-		os.Getenv("BOT_TOKEN"),
-	)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	log.Println("Bot Online")
-
-	_ = pool
-	_ = bot
+	fmt.Println("Bot Online")
 }
