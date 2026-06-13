365a49d29ec7faec52bc503148ab98d353d6c0d9
satpamrobot-beep <satpamrobot@gmail.com>
2026-06-13 20:33:57 +0700

    Create bot.go

---
+++ internal/bot/bot.go
@@ -0,0 +1,39 @@
+package bot
+
+import (
+	"log"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+)
+
+type Bot struct {
+	api *tgbotapi.BotAPI
+}
+
+func NewBot(token string) (*Bot, error) {
+	api, err := tgbotapi.NewBotAPI(token)
+	if err != nil {
+		return nil, err
+	}
+
+	api.Debug = false
+
+	log.Println("Bot authorized:", api.Self.UserName)
+
+	return &Bot{api: api}, nil
+}
+
+func (b *Bot) Start() {
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates := b.api.GetUpdatesChan(u)
+
+	for update := range updates {
+		if update.Message == nil {
+			continue
+		}
+
+		log.Println("Message:", update.Message.Text)
+	}
+}
